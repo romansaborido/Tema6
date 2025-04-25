7bb24ae78c70c40bb83cb7c1912032dcc12a0e81
romansaborido <roman.saborido@iesnervion.es>
2025-04-25 14:38:17 +0200

    Ejercicio 7

--- Tema6/src/ejercicio7/Main.java
+++ Tema6/src/ejercicio7/Main.java
@@ -1,7 +1,14 @@
 package ejercicio7;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
 
+	public static final String agenda = "src\\ejercicio7\\Agenda.txt";
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
@@ -12,6 +19,26 @@ public class Main {
 		System.out.println("2. Buscar por nombre");
 		System.out.println("3. Mostrar todos");
 		System.out.println("4. Salir");
+	}
+	
+	static void cargarDatos() {
+		
+		// Leemos el fichero con el br
+		try (BufferedReader br = new BufferedReader(new FileReader(agenda))) {
+		
+			
+			
+			// Obtenemos la primera linea
+			String linea = br.readLine();
+			
+			// Almacenamos los datos en un array
+			
+		// Capturamos las excepciones y mostramos un mensaje de error
+		} catch (FileNotFoundException e) {
+			System.out.println("No se ha podido encontrar el archivo indicado");
+		} catch (IOException e) {
+			System.out.println("No se ha podido modificar el archivo indicado");
+		}
 	}
 
 }
