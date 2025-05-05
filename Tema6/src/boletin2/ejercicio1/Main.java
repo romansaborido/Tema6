6e04c0c9e5a5da522231082b17dbfc0f41e295fe
romansaborido <roman.saborido@iesnervion.es>
2025-05-05 12:13:47 +0200

    BOLETIN 2 initial

--- Tema6/src/boletin2/ejercicio1/Main.java
+++ Tema6/src/boletin2/ejercicio1/Main.java
@@ -1,10 +1,48 @@
 package boletin2.ejercicio1;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		// Creamos el scanner para realizar el conteo de caracteres
+		Scanner reader = new Scanner(System.in);
+		
+		// Variable para obtener cada linea del fichero
+		String linea;
+		
+		// Array para almacenar cada palabra que contiene cada linea
+		String palabras[];
+		
+		// Leemos el fichero carta.txt
+		try (BufferedReader br = new BufferedReader(new FileReader ("src\\boletin2\\ejercicio1\\Carta.txt"))) {
+			
+			// Leemos la primera linea del fichero
+			linea = br.readLine();
+			
+			// Obtenemos las palabras por separado
+			palabras = linea.split(" ");
+			
+			// Recorremos cada palabra de la linea y contamos los tipos de caracteres
+			for (int i = 0 ; i < palabras.length ; i++) {
+				
+				//
+			}
+			
+		// Capturamos las excepciones y mostramos un mensaje de error
+		} catch (FileNotFoundException e) {
+			System.out.println("El archivo indicado no se pudo encontrar");
+		} catch (IOException e) {
+			System.out.println("Ha ocurrido un error al leer el archivo indicado");
+		}
+
+		// Cerramos el scanner
+		reader.close();
 	}
 
 }
